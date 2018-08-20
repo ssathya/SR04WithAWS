d0097bdccc97f19542c6c1149fe4debf07fbb6ef
Sridhar Sathya <sridhar_0314@live.com>
2018-08-20 14:09:11 -0400

    LED blink success.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
+//includes
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+
+//defines
 #define OnBoardLED 2
+
+//variables
+bool ledStatus;
 void setup() {
     // put your setup code here, to run once:
+    pinMode(OnBoardLED, OUTPUT);
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
+    ledStatus = ledStatus == true ? false : true;
+    digitalWrite(OnBoardLED, ledStatus == true ? HIGH : LOW);
+    delay(1000);
 }
