6661471489f71058351029178f64b40239c1cdf7
Sridhar Sathya <sridhar_0314@live.com>
2018-08-20 14:06:15 -0400

    Initial commit. Template project

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#define OnBoardLED 2
+void setup() {
+    // put your setup code here, to run once:
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
